565812ecb9b01401d75acc6466e5c29cb2aa18fe
Bas-sheikh <basilelshakhe@gmail.com>
2022-08-30 13:44:33 +0300

    create and require all package. relates #6

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const { join } = require('path');
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const router = require('./routers');
+
+const app = express();
+
+app.use(cookieParser())
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(join(__dirname, '..', 'public')));
+app.use(router);
+
+app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+  res.sendStatus(404)
+});
+
+app.use((err, req, res, next) => {
+  res.sendStatus(500)
+});
+
+module.exports = app;
