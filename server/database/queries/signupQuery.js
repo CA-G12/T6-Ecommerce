8ff7d0f2714b0b2af5da029053fc225ca8b2be32
Rabee <ralbhaisi96@gmail.com>
2022-08-31 09:42:06 +0300

    Fix some Eslint issues

--- server/database/queries/signupQuery.js
+++ server/database/queries/signupQuery.js
@@ -1,10 +1,8 @@
-const connection = require("../config/connection");
+const connection = require('../config/connection');
 
-const postSignupQuery = (data) => {
-  return connection.query(
-    "insert into users(first_name, last_name, email, password) values($1, $2, $3, $4)",
-    data
-  );
-};
+const postSignupQuery = (data) => connection.query(
+  'insert into users(first_name, last_name, email, password) values($1, $2, $3, $4)',
+  data,
+);
 
 module.exports = postSignupQuery;
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,8 @@
-const { Router } = require("express");
-const { signIn, postSignup, getProductsCart } = require("../controllers");
+const { Router } = require('express');
+const { signIn, postSignup, getProductsCart } = require('../controllers');
 
 const router = Router();
-router.get("/usercart", getProductsCart);
-router.post("/signin", signIn);
-router.post("/signup", postSignup);
+router.get('/usercart', getProductsCart);
+router.post('/signin', signIn);
+router.post('/signup', postSignup);
 module.exports = router;
